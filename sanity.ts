93edc9e067d59ebd30cf80ac6cbcbc4d656c3a1c
Viktor Sopko <najetak.viktrixeu@gmail.com>
2022-09-18 10:43:57 +0200

    Fix sanity client error.

--- sanity.ts
+++ sanity.ts
@@ -1,6 +1,8 @@
 import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
+console.log();
+
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
@@ -12,4 +14,4 @@ export const config = {
 export const sanityClient = createClient(config);
 
 export const urlFor = (source: any) =>
-  createImageUrlBuilder(config).image(source);
+  createImageUrlBuilder(sanityClient).image(source);
